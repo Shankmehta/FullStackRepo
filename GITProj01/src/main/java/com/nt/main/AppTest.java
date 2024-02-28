4b8361acda1062c246659a69422aaea4587c9d32
Ankit Solanki <109839582+Ankit7999@users.noreply.github.com>
2024-02-28 12:29:58 +0530

    Task 1 from Dev1

--- GITProj01/src/main/java/com/nt/main/AppTest.java
+++ GITProj01/src/main/java/com/nt/main/AppTest.java
@@ -1,5 +1,13 @@
 package com.nt.main;
 
-public class AppTest {
+import com.nt.service.ArithmeticOperations;
 
+public class AppTest 
+{
+   public static void main(String args[])
+   {
+	   ArithmeticOperations ar = new ArithmeticOperations();
+	  System.out.println("sum:"+ar.sum(100,200));
+	  System.out.println("sub:"+ar.sub(100,200));
+   }
 }
---
+++ GITProj01/src/main/java/com/nt/service/ArithmeticOperations.java
@@ -0,0 +1,17 @@
+package com.nt.service;
+
+/**
+ * Hello world!
+ *
+ */
+public class ArithmeticOperations 
+{
+    public int sum(int x,int y)
+    {
+    	return x+y;
+    }
+    public int sub(int x,int y)
+    {
+    	return x-y;
+    }
+}
