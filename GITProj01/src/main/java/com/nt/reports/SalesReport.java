d49237f4cf5a6ed61ade01ac4bf48c9b051093c9
Ankit Solanki <109839582+Ankit7999@users.noreply.github.com>
2024-02-29 18:35:30 +0530

    Dev1--b2--task1

--- GITProj01/src/main/java/com/nt/reports/SalesReport.java
+++ GITProj01/src/main/java/com/nt/reports/SalesReport.java
@@ -1,5 +1,9 @@
 package com.nt.reports;
 
-public class SalesReport {
-
+public class SalesReport 
+{
+   public void weeklyReport()
+   {
+	   System.out.println("SalesReport.weeklyReport()");
+   }
 }
